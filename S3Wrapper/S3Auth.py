985ef82f57b1374752d9f8dc9aa9548ef6464f3d
l02 <l8556@outlook.com>
2024-03-25 19:06:12 +0300

    init

--- S3Wrapper/S3Auth.py
+++ S3Wrapper/S3Auth.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-from os.path import expanduser, join, isfile
-from io import open as io_open
-
+import os
 import boto3
 
 
 class S3Auth:
+    """
+    Class for managing authentication credentials for AWS S3.
+    """
 
     def __init__(self, region_name: str, access_key: str = None, secret_access_key: str = None):
         self.region_name = region_name
-        self.client = self._client(access_key, secret_access_key)
+        self.client = self._create_client(access_key, secret_access_key)
 
-    def _client(self, access_key: str = None, secret_access_key: str = None) -> boto3.client:
+    def _create_client(self, access_key: str = None, secret_access_key: str = None) -> boto3.client:
+        """
+        Initialize an S3 client with provided or read access keys.
+        :param access_key: AWS access key.
+        :param secret_access_key: AWS secret access key.
+        :return: Initialized boto3 S3 client.
+        """
         if not access_key or not secret_access_key:
             access_key, secret_access_key = self._read_keys()
         return boto3.client(
@@ -21,18 +28,28 @@ class S3Auth:
             region_name=self.region_name
         )
 
-    @staticmethod
-    def _read_keys(key_location = f'{expanduser("~")}/.s3') -> tuple[str, str]:
-        access_key_id, secret_access_key = join(key_location, 'key'), join(key_location, 'private_key')
-        if not isfile(access_key_id) or not isfile(secret_access_key):
-            raise print(
-                f"[red]|ERROR| No key or private key found in {key_location} "
-                f"Please create files {access_key_id} "
-                f"and {secret_access_key}"
-            )
-        return S3Auth._file_read(access_key_id), S3Auth._file_read(secret_access_key)
+    def _read_keys(self, key_location: str = os.path.join(os.path.expanduser("~"), '.s3')) -> tuple[str, str]:
+        """
+        Read AWS access and secret access keys from the specified location.
+        :param key_location: Location to read the keys from.
+        :return: Tuple containing access key and secret access key.
+        """
+        access_key_path = os.path.join(key_location, 'key')
+        secret_access_key_path = os.path.join(key_location, 'private_key')
 
-    @staticmethod
-    def _file_read(path: str) -> str:
-        with io_open(path, 'r', encoding='utf-8') as file:
+        if not all(map(os.path.isfile, [access_key_path, secret_access_key_path])):
+            raise FileNotFoundError(
+                f"[red]|ERROR| No key or private key found in {key_location}. "
+                f"Please create files {access_key_path} and {secret_access_key_path}."
+            )
+
+        return self._file_read(access_key_path), self._file_read(secret_access_key_path)
+
+    def _file_read(self, path: str) -> str:
+        """
+        Read contents of a file.
+        :param path: Path of the file to read.
+        :return: Contents of the file.
+        """
+        with open(path, 'r', encoding='utf-8') as file:
             return file.read().strip()
--- S3Wrapper/S3Wrapper.py
+++ S3Wrapper/S3Wrapper.py
@@ -10,34 +10,59 @@ from rich import print
 class S3Exception(Exception): ...
 
 class S3Wrapper:
+    """
+    Wrapper class for interacting with AWS S3.
+    """
 
     def __init__(
             self,
             bucket_name: str = 'conversion-testing-files',
-            region: str ='us-east-1',
+            region: str = 'us-east-1',
             access_key: str = None,
             secret_access_key: str = None
     ):
+        """
+        Initialize the S3Wrapper.
+        :param bucket_name: Name of the S3 bucket.
+        :param region: AWS region.
+        :param access_key: AWS access key.
+        :param secret_access_key: AWS secret access key.
+        """
         self.s3 = S3Auth(region, access_key, secret_access_key).client
-        self.bucket = self._get_bucket(bucket_name)
+        self.bucket = self._check_bucket_name(bucket_name)
 
     def get_files(self, s3_dir: str = None) -> list:
+        """
+        Get a list of files in the S3 bucket.
+        :param s3_dir: Optional directory path in the S3 bucket.
+        :return: List of file names.
+        """
         s3_objects = self.get_objects()
         if isinstance(s3_dir, str):
             return [file for file in s3_objects if file.startswith(s3_dir) and not file.endswith('/')]
         return [file for file in s3_objects if not file.endswith('/')]
 
     def get_objects(self) -> list:
+        """
+        Get a list of all objects in the S3 bucket.
+        :return: List of object keys.
+        """
         file_names = []
         for page in self.s3.get_paginator('list_objects_v2').paginate(Bucket=self.bucket):
             if 'Contents' in page:
-                for obj in page['Contents']:
-                    file_names.append(obj['Key'])
+                file_names.extend([obj['Key'] for obj in page['Contents']])
         if not file_names:
             print("[red]|INFO| Bucket is empty.")
         return file_names
 
     def download(self, object_key: str, download_path: str, stdout: bool = True) -> bool:
+        """
+        Download an object from the S3 bucket to a local path.
+        :param object_key: Key of the object in the S3 bucket.
+        :param download_path: Local path to download the object.
+        :param stdout: Whether to print download information.
+        :return: True if download is successful, False otherwise.
+        """
         print(f"[green]|INFO| Downloading {self.bucket}/{object_key} to {download_path}") if stdout else None
         try:
             self.s3.download_file(self.bucket, object_key, download_path)
@@ -47,10 +72,22 @@ class S3Wrapper:
             return False
 
     def upload(self, file_path: str, object_key: str,  stdout: bool = True) -> None:
+        """
+        Upload a file to the S3 bucket.
+        :param file_path: Local path of the file to upload.
+        :param object_key: Key of the object in the S3 bucket.
+        :param stdout: Whether to print upload information.
+        """
         print(f"[green]|INFO| Uploading {basename(file_path)} to {self.bucket}/{object_key}") if stdout else None
         self.s3.upload_file(file_path, self.bucket, object_key)
 
     def get_headers(self, object_key: str, stderr: bool = True) -> bool | dict:
+        """
+        Get the headers of an object in the S3 bucket.
+        :param object_key: Key of the object in the S3 bucket.
+        :param stderr: Whether to print error messages.
+        :return: Headers of the object if it exists, False otherwise.
+        """
         try:
             return self.s3.head_object(Bucket=self.bucket, Key=object_key)
         except self.s3.exceptions.ClientError:
@@ -61,18 +98,33 @@ class S3Wrapper:
             return False
 
     def get_size(self, object_key: str) -> str | int:
+        """
+        Get the size of an object in the S3 bucket.
+        :param object_key: Key of the object in the S3 bucket.
+        :return: Size of the object.
+        """
         headers = self.get_headers(object_key)
         return headers['ContentLength'] if headers else 0
 
     def get_sha256(self, object_key: str) -> str | None:
+        """
+        Get the SHA256 hash of an object in the S3 bucket.
+        :param object_key: Key of the object in the S3 bucket.
+        :return: SHA256 hash of the object.
+        """
         try:
             file_contents = self.s3.get_object(Bucket=self.bucket, Key=object_key)['Body'].read()
             return hashlib.sha256(file_contents).hexdigest()
         except Exception as e:
-            print(f"Error while retrieving a file from S3: {e}")
+            print(f"[red]|ERROR| Error while retrieving a file from S3: {e}")
             return None
 
     def delete(self, object_key: str, warning_msg: bool = True) -> None:
+        """
+        Delete an object from the S3 bucket.
+        :param object_key: Key of the object in the S3 bucket.
+        :param warning_msg: Whether to display a warning message before deletion.
+        """
         print(f"[red]|INFO| Deleting object: {object_key} from {self.bucket}")
         Prompt.ask(f"[bold red]|WARNING|Are you sure you want to delete the object:") if warning_msg else None
         if self.get_headers(object_key):
@@ -81,18 +133,31 @@ class S3Wrapper:
             print(f"[bold red]|ERROR| Can't delete object: {object_key}")
 
     def delete_from_list(self, object_keys: list) -> None:
+        """
+        Delete multiple objects from the S3 bucket.
+        :param object_keys: List of object keys to delete.
+        """
         print(f"[red]|INFO| List of objects to be removed from the bucket {self.bucket}: {object_keys}")
-        if Prompt.ask(f"[bold red]|WARNING| Continue?", choices=['yes', 'no'], default='no') == 'yes':
+        if Prompt.ask("[bold red]|WARNING| Continue?", choices=['yes', 'no'], default='no') == 'yes':
             for obj in object_keys:
                 self.delete(obj, warning_msg=False)
 
     def buckets_list(self) -> list:
+        """
+        Get a list of all buckets in the S3 service.
+        :return: List of bucket names.
+        """
         try:
             return [bucket['Name'] for bucket in self.s3.list_buckets()['Buckets']]
         except KeyError:
-            raise S3Exception(f"[red]|ERROR| Error while getting bucket list from aws.")
+            raise S3Exception(f"[red]|ERROR| Error while getting bucket list from AWS.")
 
-    def _get_bucket(self, bucket_name: str) -> str | None:
+    def _check_bucket_name(self, bucket_name: str) -> str | None:
+        """
+        Check if the specified bucket exists in the S3 service.
+        :param bucket_name: Name of the S3 bucket.
+        :return: Name of the bucket if it exists, None otherwise.
+        """
         if bucket_name in self.buckets_list():
             return bucket_name
         raise S3Exception(f"[red]|ERROR| Bucket {bucket_name} not found.")
